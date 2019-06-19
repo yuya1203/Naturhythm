4d53e18652c26881a480b9ed470b3ecf5eba5ed5
yuya1203 <yuyaaa.1203@gmail.com>
2019-06-19 23:20:41 +0900

    heroku

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -57,7 +57,7 @@ class MicropostsController < ApplicationController
   def destroy
     @micropost.destroy
     flash[:success] = 'メッセージを削除しました。'
-    redirect_back(fallback_location: root_path)
+    redirect_to root_url
   end
 
   def search
